acb221d58f024039cfbf32fa7e6bb46d048e483d
Ainur <ainur.hak2004@gmail.com>
2022-01-18 14:05:40 +0300

    death registration

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -20,18 +20,43 @@ namespace ConsoleApp3
             Attack(archer, warrior);
             Attack(archer, warrior);
             Attack(archer, warrior);
+            Attack(archer, warrior);
+            Attack(archer, warrior);
+            Attack(archer, warrior);
+            Attack(archer, warrior);
+            Attack(archer, warrior);
+            Attack(archer, warrior);
+            Attack(archer, warrior);
+            Attack(archer, warrior);
+            Attack(archer, warrior);
+            Attack(archer, warrior);
+            Attack(archer, warrior);
+            Attack(archer, warrior);
+            Attack(archer, warrior);
+            Attack(archer, warrior);
+            Attack(archer, warrior);
 
             Heal(cleric, warrior);
         }
 
         public static void Attack(Character attackingCharacter, Character attackedCharacter)
         {
-            attackedCharacter.Damage(attackingCharacter.Attack);
+            if (attackingCharacter.Health <= 0)
+                Console.WriteLine("Вы мертвы, действие невозможно.");
+            else if (attackedCharacter.Health <= 0)
+                Console.WriteLine("Враг уже мертв!");
+            else
+                attackedCharacter.Damage(attackingCharacter.Attack);
         }
 
         public static void Heal(Character healingCharacter, Character healedCharacter)
         {
-            healedCharacter.Heal((healingCharacter as Cleric).Healing);
+            if (healingCharacter.Health <= 0)
+                Console.WriteLine("Вы мертвы, действие невозможно.");
+            else if (healedCharacter.Health <= 0)
+                Console.WriteLine("Тело мертво... Может помочь черная магия.");
+            else
+                healedCharacter.Heal((healingCharacter as Cleric).Healing);
         }
         public static void ShowMessage(string mes)
         {
