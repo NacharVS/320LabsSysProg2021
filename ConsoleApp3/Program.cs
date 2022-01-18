3b30252b4d13413df620c2e00b2d9bac430643c4
r3dinc <r3dinc@yandex.ru>
2022-01-18 12:21:42 +0300

    start

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student std = new Student();
+            std.Name = "Ivan";
+            std.Group = 333;
+            Console.WriteLine(std.Group);
+            std.GroupChandgedEvent += ShowMessage;
+            std.GroupChandgedEvent += ShowMessage1;
+            std.Group = 331;
+            Console.WriteLine(std.Group);
+        }
+
+         static void ShowMessage(string mes)
+        {
+            Console.WriteLine(mes);
+        }
+        static void ShowMessage1(string mes)
+        {
+            Console.WriteLine(mes+mes);
+        }
+    }
+}
