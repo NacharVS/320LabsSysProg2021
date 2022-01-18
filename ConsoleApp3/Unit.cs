a2f303d0a1e2e108cfee9372f9e9aa586bd64d77
linar228 <linar369@mail.ru>
2022-01-18 13:22:42 +0300

    finish unit

--- ConsoleApp3/Unit.cs
+++ ConsoleApp3/Unit.cs
@@ -6,12 +6,27 @@ namespace ConsoleApp3
 {
     abstract class Unit
     {
+		public delegate void HealthChangedDelegate(string mes);
+		public event HealthChangedDelegate HealthChangedEvent;
+
 		private double _health;
 
 		public double Health
 		{
 			get { return _health; }
-			set { _health = value; }
+			set { if (value > 0)
+					{
+						_health = value;
+						HealthChangedEvent?.Invoke($"{_health}");
+					};
+			}
+		}
+
+		private double _damage;
+		public double Damage
+		{
+			get { return _damage; }
+			set { _damage = value; }
 		}
 
 	}
