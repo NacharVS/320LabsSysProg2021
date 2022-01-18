537131fa9f9fc17f4d2cb75ca8078ffebd632e0b
KhairullinAdel <82815241+KhairullinAdel@users.noreply.github.com>
2022-01-18 13:33:27 +0300

    Bishop is created

---
+++ ConsoleApp3/Bishop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    internal class Bishop : Unit
+    {
+        public double HealAmount { get; set; }
+
+        public Bishop(double hitPoints, double healthPower)
+        {
+            HealAmount = healthPower;
+            this.Health = hitPoints;
+        }
+        public void Heal(Unit u)
+        {
+            if (u.Health > 0)
+            {
+                u.Health += this.HealAmount;
+            }
+        }
+    }
+}
--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -9,12 +9,16 @@ namespace ConsoleApp3
         {
             Warrior warrior = new Warrior(50);
             Peasant peasant = new Peasant(100);
+            Bishop bishop = new Bishop(100, 20);
             
             warrior.HealthChangedEvent += ShowMessage;
             peasant.HealthChangedEvent += ShowMessage;
+            bishop.HealthChangedEvent += ShowMessage;
 
             Console.WriteLine(peasant.Health);
-            warrior.Attack(peasant, 110);
+            warrior.Attack(peasant, 70);
+            Console.WriteLine(peasant.Health);
+            bishop.Heal(peasant);
             Console.WriteLine(peasant.Health);
         }
 
