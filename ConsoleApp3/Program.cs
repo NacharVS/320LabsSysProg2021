4053234a448fe6dee253d918e47b0823ad0b37df
r3dinc <r3dinc@yandex.ru>
2022-01-18 14:38:33 +0300

    fix Task

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -11,6 +11,8 @@ namespace ConsoleApp3
         }
         // 1. create methods for imitation of battle between 2 units. All nonBuildingsunits can attack each other
         // 2. only catapult can attack building
+        // 3. Units: Soldier(Only mlee attack), Archer(RangeAttack (has 5 arrows)),
+        // All units Has AttackSpeed/WalkingSpeed. 
          
 
     }
