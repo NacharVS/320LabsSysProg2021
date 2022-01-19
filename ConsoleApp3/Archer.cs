00b6993509be4ac177e5e8fe55e98b7b2a3ed882
lemonjuice <secondemilbarish@gmail.com>
2022-01-19 17:08:28 +0300

    add archer

---
+++ ConsoleApp3/Archer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class Archer : Unit
+    {
+        public Archer(string name) : base(name, 2000, 3, 2.5, 3)
+        {
+
+        }
+    }
+}
