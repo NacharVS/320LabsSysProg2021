306b61ced7b1834ba11ca5cb99ef14af990b03f4
Егор Артемьев <batman_batman_1910@mail.ru>
2022-01-20 08:33:56 +0300

    edit unit and warrior

--- ConsoleApp3/Unit.cs
+++ ConsoleApp3/Unit.cs
@@ -7,12 +7,27 @@ namespace ConsoleApp3
     abstract class Unit
     {
 		private double _health;
+		public delegate void HpChangedDelegate(string message);
 
-		public double Health
+		public double Hp
 		{
 			get { return _health; }
-			set { _health = value; }
+			set
+			{
+				if (value > 0)
+				{
+					HpChangedEvent?.Invoke($"HealthPoints has changed {value - _health}");
+					_health = value;
+
+				}
+				else
+				{
+					_health = 0;
+					HpChangedEvent?.Invoke("Dead");
+				}
+			}
 		}
+		public event HpChangedDelegate HpChangedEvent;
 
 	}
 }
--- ConsoleApp3/Warrior.cs
+++ ConsoleApp3/Warrior.cs
@@ -6,5 +6,19 @@ namespace ConsoleApp3
 {
     class Warrior : Unit
     {
+        public void Attack(Unit unit, double damage)
+        {
+            if (damage >= 0)
+                unit.Hp -= damage;
+
+        }
+        public Warrior()
+        {
+            Hp = 90;
+        }
+        public Warrior(double health)
+        {
+            Hp = health;
+        }
     }
 }
