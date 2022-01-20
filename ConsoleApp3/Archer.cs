20b9c1d75d6afde25b08acb586d2388eead58cf7
линар валиев <linar369@mail.ru>
2022-01-21 00:52:15 +0300

    add Archer and Building

---
+++ ConsoleApp3/Archer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class Archer : BattleUnit
+    {
+        private int _minRangeDamage;
+        private int _maxRangeDamage;
+        private int _arrows;
+
+        public Archer(string name, double health, int AttackSpeed, double maxH, int min, int max) : base(name, health, AttackSpeed, maxH, min, max)
+        {
+            _minRangeDamage = 4;
+            _maxRangeDamage = 12;
+            _arrows = 5;
+        }
+
+        public double RangeAttack(Random rnd)
+        {
+            if (_arrows > 0)
+            {
+                double currentDamage = Convert.ToDouble(rnd.Next(_minRangeDamage, _maxRangeDamage));
+                return currentDamage;
+            }
+            else
+            {
+                return MleeAttack(rnd);
+            }
+        }
+}
---
+++ ConsoleApp3/Building.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class Building : Unit
+    {
+        private double _wall;
+        public double Wall { get => _wall; set => _wall = value; }
+        public Building(string name, double health) : base(name, health)
+        {
+            Wall = health;
+        }
+        public override void Info()
+        {
+            Console.WriteLine($"Building {Name} has {_wall} walls and {Health}hp");
+        }
+    }
+}
