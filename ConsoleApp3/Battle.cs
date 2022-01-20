387c9e5b1f38252d77ee3f50ac8df76d1fa30943
Даниль Фахрутдинов <danilfahrutdinov357@mail.ru>
2022-01-20 09:02:25 +0300

    add battle

---
+++ ConsoleApp3/Battle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class Battle : Unit
+    {
+        private double _attackSpeed;
+        private double _walkingSpeed;
+
+        public Battle(string name, double health, double damage, double _attackSpeed, double _walkingSpeed) : base(name, health, damage)
+        {
+            this._attackSpeed = _attackSpeed;
+            this._walkingSpeed = _walkingSpeed;
+        }
+
+        public virtual double Infighting()
+        {
+            return 3;
+        }
+    }
+}
