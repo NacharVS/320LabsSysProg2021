6a12c0171141cbe30f637c35d769a40eb8637571
Iskevul <74836797+Iskevul@users.noreply.github.com>
2022-01-25 14:24:22 +0300

    Таймер

---
+++ Timer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Timer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task timer1 = new Task(() => TimeWait(10));
+            Task timer2 = new Task(() => TimeWait(10));
+
+            Task timer5 = new Task(() => TimeWait(10));
+            Task timer6 = new Task(() => TimeWait(10));
+
+            Task timer3 = timer1.ContinueWith(t => TimeWaitHalf(6, timer5));
+            Task timer4 = timer2.ContinueWith(t => TimeWaitHalf(6, timer6));
+
+            timer1.Start();
+            timer2.Start();
+
+            Console.ReadLine();
+        }
+        public static void TimeWait(int sec)
+        {
+            Thread.Sleep(sec * 1000);
+            Console.WriteLine($"{sec} seconds have passed");
+        }
+
+        public static void TimeWaitHalf(int sec, Task timer)
+        {
+            Thread.Sleep(sec / 2 * 1000);
+            timer.Start();
+            Console.WriteLine($"{sec / 2} seconds have passed");
+            Thread.Sleep(sec / 2 * 1000);
+            Console.WriteLine($"{sec} seconds have passed");
+        }
+    }
+}
