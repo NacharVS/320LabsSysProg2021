bf707d77b35feb9f4089fbf2cd48fd9483b7e683
201913 <201913@1220-09>
2022-01-25 14:58:50 +0300

    Add timer

---
+++ TimerConsole/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Task timer1 = new Task(() => StartTimer(10, "timer1", 0));
+            Task timer2 = new Task(() => StartTimer(10, "timer2", 0));
+
+            Task timer3 = timer1.ContinueWith(time => StartTimer(10, "timer3", 0));
+            Task timer4 = timer2.ContinueWith(time => StartTimer(10, "timer4", 0));
+
+            Task timer5 = timer1.ContinueWith(time => StartTimer(10, "timer5", 5));
+            Task timer6 = timer2.ContinueWith(time => StartTimer(10, "timer6", 5));
+
+            timer1.Start();
+            timer2.Start();
+
+            Console.ReadKey();
+        }
+        static void StartTimer(int time, string timer, int delayTime)
+        {
+            Thread.Sleep(delayTime * 1000);
+            while (time > 0)
+            {
+                Console.WriteLine($"{timer} {time}");
+                Thread.Sleep(1000);
+                time--;
+            }
+
+            Console.WriteLine($"{timer} Время закончилось");
+        }
+
+    }
+}
---
+++ TimerWPF/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TimerWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnParallelStart_Click(object sender, RoutedEventArgs e)
+        {
+            timer1Time.Text = "weaggggggg";
+            Task timer1 = new Task(() => StartTimerAsync(10, "timer1", 0, timer1Time));
+            Task timer2 = new Task(() => StartTimerAsync(10, "timer2", 0, timer2Time));
+            timer1.Start();
+            timer2.Start();
+            StartTimerAsync(10, "timer1", 0, timer1Time);
+        }
+
+        private void btnSequentiallyStart_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private static void StartTimer(int time, string timer, int delayTime, TextBlock textBlock)
+        {
+            Thread.Sleep(delayTime * 1000);
+            while (time > 0)
+            {
+                textBlock.Text = $"{timer} {time}";
+                Thread.Sleep(1000);
+                time--;
+            }
+
+            textBlock.Text = $"{timer} Время закончилось";
+        }
+        private static async void StartTimerAsync(int time, string timer, int delayTime, TextBlock textBlock)
+        {
+            await Task.Run(() => StartTimer(time, timer, delayTime, textBlock));
+        }
+    }
+}
