205c4f9e5789df092604b0b385abd5ef104af58f
bozinandrey1@yandex.ru <bozinandrey1@yandex.ru>
2022-01-27 09:30:23 +0300

    fix

--- TimerWPF/MainWindow.xaml.cs
+++ TimerWPF/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace TimerWPF
 {
@@ -30,42 +31,34 @@ namespace TimerWPF
         {
             try
             {
-                int timer1Time = int.Parse(tbTimer1Set.Text);
-                int timer2Time = int.Parse(tbTimer2Set.Text);
+                int timerTime = int.Parse(tbTimerSet.Text);
+                while (timerTime > 0)
+                {
+                    --timerTime;
 
-                await Task.Run(() => StartTimer(timer1Time, tbTimer1Value));
-                await Task.Run(() => StartTimer(timer2Time, tbTimer2Value));
+                    string time = timerTime.ToString();
+                    if (time.Length == 1)
+                        tbTimerValue.Text = $"00:00:0{time}";
+                    else if (time.Length == 2)
+                        tbTimerValue.Text = $"00:00:{time}";
+
+                    Thread.Sleep(1000);
+                }
+                MessageBox.Show("Таймер завершил работу");
             }
             catch (FormatException)
             {
-                MessageBox.Show("Введите число");
+                MessageBox.Show("Введено не числовое значение");
             }
         }
 
-        private void btnPar_Click(object sender, RoutedEventArgs e)
+        private async void btnPar_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                int timer1Time = int.Parse(tbTimer1Set.Text);
-                int timer2Time = int.Parse(tbTimer2Set.Text);
-
-                while (timer1Time > 0 || timer2Time > 0)
-                {
-                    if (timer1Time > 0)
-                    {
-                        --timer1Time;
-                    }
-                    if (timer2Time > 0)
-                    {
-                        --timer2Time;
-                    }
-
-                    tbTimer1Value.Text = timer1Time.ToString();
-                    tbTimer2Value.Text = timer2Time.ToString();
-                    Thread.Sleep(1000);
-                }
-
-                MessageBox.Show("Таймеры завершили работу");
+                int timerTime = int.Parse(tbTimerSet.Text);
+                await Task.Run(() => StartTimer(timerTime));
+                MessageBox.Show("Таймер завершил работу");
             }
             catch
             {
@@ -73,12 +66,21 @@ namespace TimerWPF
             }
         }
 
-        public static void StartTimer(int time, TextBlock textBlock)
+        public void StartTimer(int timerTime)
         {
-            while (time > 0)
+            while (timerTime > 0)
             {
-                textBlock.Text = time.ToString();
-                --time;
+                --timerTime;
+
+                Dispatcher.Invoke((Action)(() =>
+                {
+                    string time = timerTime.ToString();
+                    if (time.Length == 1)
+                        tbTimerValue.Text = $"00:00:0{time}";
+                    else if (time.Length == 2)
+                        tbTimerValue.Text = $"00:00:{time}";
+                }));
+
                 Thread.Sleep(1000);
             }
         }
