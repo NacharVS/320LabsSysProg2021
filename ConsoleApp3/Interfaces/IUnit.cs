c9fa2841bf6825a33fe24dbf1bf27f53dca978f8
linar228 <linar369@mail.ru>
2022-02-04 14:18:28 +0300

    start interfaces

---
+++ ConsoleApp3/Interfaces/IUnit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3.Interfaces
+{
+    public interface IUnit
+    {
+        string Name { get; set; }
+        double Health { get; set; }
+        double MaxHealth { get; set; }
+    }
+}
