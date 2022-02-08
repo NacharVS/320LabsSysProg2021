1cd069b4a0cab194426265d19751f0f5dff7b8e3
r3dinc <r3dinc@yandex.ru>
2022-02-08 13:10:45 +0300

    async

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -6,19 +6,66 @@ namespace ConsoleApp3
 {
     class Program
     {
+        static string MakeCream()
+        {
+            Thread.Sleep(5000);
+            Console.WriteLine("cream ready");
+            return "CreamForCake";
+        }
+        static async Task MakeCreamAsync()
+        {
+            await Task.Run(() => MakeCream());
+        }
+
+        static string MakeChicken()
+        {
+            Console.WriteLine("chicken ready");
+            return "Chicken";
+        }
+        static string MakeKebab()
+        {
+            Thread.Sleep(7000);
+            Console.WriteLine("kebab ready");
+            return "kebab";
+        }
+
+        static async Task MakeCakeAsync()
+        {
+
+        }
+
+        static async Task MakeKebabAsync()
+        {
+           await Task.Run(() => MakeKebab());
+        }
+        static string MakeKorjes()
+        {
+            Thread.Sleep(10000);
+            Console.WriteLine("korjes ready");
+            return "korjes";
+        }
+
+        static async Task MakeKorjesAsync()
+        {
+            await Task.Run(() => MakeKorjes());
+        }
+        static string MakeCake()
+        {
+            MakeKebabAsync().GetAwaiter();
+            MakeCreamAsync().GetAwaiter();
+            MakeKorjesAsync().GetAwaiter().GetResult();
+            Console.WriteLine("cake ready");
+            return "cake";
+        }
+        static void Cleaning()
+        {
+            Console.WriteLine("Cleaning comlete");
+        }
         static void Main(string[] args)
         {
-            Soldier Bob = new Soldier(new Sword());
-            Bob.weapon.InflictDamage();
-            Blacksmith Ivan = new Blacksmith();
-            var weaponX = Ivan.MakeBow();
-            Bob.weapon = weaponX;
-            Bob.weapon.InflictDamage();
-            var weaponY = Ivan.MakeJavelin();
-            Bob.weapon = weaponY;
-            Bob.weapon.InflictDamage();
-            Bob.Throw(weaponY);
-            //переделать стратегию через интерефейсы
+            MakeCake();
+
+            Console.ReadKey();
 
 
 
