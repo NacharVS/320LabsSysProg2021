67eda40c3153666a265a9f539ccbe1e28267cfed
201913 <201913@1220-09>
2022-02-08 15:06:49 +0300

    Add data display

---
+++ DisplayData/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DisplayData
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var rnd = new Random();
+            for (int i =0; i<5; i++)
+            {
+                await Task.Delay(1000);
+                Dispatcher.Invoke(() => Numbers.Items.Add(rnd.Next(10, 1000)));
+            }
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Numbers.Items.Clear();
+        }
+    }
+}
