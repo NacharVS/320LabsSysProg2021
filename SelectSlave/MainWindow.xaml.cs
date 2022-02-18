d399f1ebb3333474bd5f6006018eb7880329a886
bozinandrey1@yandex.ru <bozinandrey1@yandex.ru>
2022-02-18 16:04:27 +0300

    Select slaves

---
+++ SelectSlave/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace SelectSlave
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MongoClient client { get; set; }
+        IMongoDatabase database { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            client = new MongoClient("mongodb://localhost");
+            database = client.GetDatabase("SlaveData");
+            this.DataContext = this;
+        }
+
+        private async void btnSelectClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var collection = database.GetCollection<Slave>("Slaves");
+
+                int slavesCount = int.Parse(tbSlavesCount.Text);
+                int group = int.Parse(tbGroup.Text);
+
+                var list = await collection.Find(x => x.IdGroup == group).ToListAsync();
+
+                Random rnd = new Random();
+                var slaves = new List<Slave>();
+
+                for (int i = 0; i < slavesCount; i++)
+                {
+                    int index = rnd.Next(0, list.Count());
+                    slaves.Add(list[index]);
+                    list.Remove(list[index]);
+                }
+
+                lbSlaves.ItemsSource = slaves;
+            }
+            catch 
+            {
+                MessageBox.Show("АБЫБКА");
+            }
+           
+        }
+
+        private async void btnAddClick(object sender, RoutedEventArgs e)
+        { 
+            var collection = database.GetCollection<Slave>("Slaves");
+            
+            Slave slave = new Slave
+            {
+                FirstName = tbFirstName.Text,
+                LastName = tbLastName.Text ,
+                IdGroup = int.Parse(tbNewGroup.Text)
+            };
+
+            await collection.InsertOneAsync(slave);
+            await collection.ReplaceOneAsync(x => );
+            tbFirstName.Text = "";
+            tbLastName.Text = "";
+        }
+    }
+}
---
+++ SelectSlave/Slave.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SelectSlave
+{
+    public class Slave
+    {
+        [BsonId]
+        [BsonIgnoreIfDefault]
+        public ObjectId _id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int IdGroup { get; set; }
+
+        public Slave() { }
+
+        public Slave(string firstName, string lastName, int group)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            IdGroup = group;
+        }
+
+        public override string ToString()
+        {
+            return $"{LastName} {FirstName}";
+        }
+    }
+}
