9fdc43ff631257c58a7ebd165f80db47447a9603
Зиля Шагиахметова <zilyasagiahmetova03@mail.ru>
2022-02-18 14:49:28 +0300

    Random

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace ConsoleApp1
+{
+    class DB
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public DB(string nameOfSubEntity, string surnameOfSubEntity)
+        {
+            NameOfSubEntity = nameOfSubEntity;
+            SurnameOfSubEntity = surnameOfSubEntity;
+        }
+
+        public string NameOfSubEntity { get; set; }
+        public string SurnameOfSubEntity { get; set; }
+    }
+    class Entity
+    {
+        public Entity(string nameOfEntity, string surnameOfSubEntity)
+        {
+            NameOfEntity = nameOfEntity;
+            SurnameOfSubEntity = surnameOfSubEntity;
+            ListOfSubEntities = new List<DB>();
+        }
+
+        public ObjectId _id { get; set; }
+        public string NameOfEntity { get; set; }
+        public string SurnameOfSubEntity { get; set; }
+        public List<DB> ListOfSubEntities { get; set; }
+    }
+    class MongoExamples
+    {
+        static Entity CreateEntity()
+        {
+            DB sub1 = new DB("Салават", "Абраров");
+            DB sub2 = new DB("Владимир", "Аввакумов");
+            DB sub3 = new DB("Егор", "Артемьев");
+            DB sub4 = new DB("Антон", "Ахтямов");
+            DB sub5 = new DB("Эмиль", "Барышев");
+            DB sub6 = new DB("Андрей", "Бозин");
+            DB sub7 = new DB("Линар", "Валиев");
+            DB sub8 = new DB("Даниил", "Гаврилов");
+            DB sub9 = new DB("Камиля", "Закирова");
+            DB sub10 = new DB("София", "Кумышбаева");
+            DB sub11 = new DB("Данис", "Мавлин");
+            DB sub12 = new DB("Иван", "Мамонов");
+            DB sub13 = new DB("Михаил", "Мясников");
+            DB sub14 = new DB("Гузель", "Насуртдинова");
+            DB sub15 = new DB("Клементий", "Николаев");
+            DB sub16 = new DB("Андрей", "Тимофеев");
+            DB sub17 = new DB("Гульназ", "Тухватуллина");
+            DB sub18 = new DB("Даниль", "Фахрутдинов");
+            DB sub19 = new DB("Адель", "Хайруллин");
+            DB sub20 = new DB("Владислав", "Чийпеш");
+            DB sub21 = new DB("Зиля", "Шагиахметова");
+            DB sub22 = new DB("Айнур", "Шакиров");
+            DB sub23 = new DB("Искандер", "Шарафеев");
+            DB sub24 = new DB("Артем", "Сематкин");
+            DB sub25 = new DB("Дияр", "Яппаров");
+
+            Entity entity1 = new Entity("AlphaEntity", "");
+            entity1.ListOfSubEntities.Add(sub1);
+            entity1.ListOfSubEntities.Add(sub2);
+            entity1.ListOfSubEntities.Add(sub3);
+            entity1.ListOfSubEntities.Add(sub4);
+            entity1.ListOfSubEntities.Add(sub5);
+            entity1.ListOfSubEntities.Add(sub6);
+            entity1.ListOfSubEntities.Add(sub7);
+            entity1.ListOfSubEntities.Add(sub8);
+            entity1.ListOfSubEntities.Add(sub9);
+            entity1.ListOfSubEntities.Add(sub10);
+            entity1.ListOfSubEntities.Add(sub11);
+            entity1.ListOfSubEntities.Add(sub12);
+            entity1.ListOfSubEntities.Add(sub13);
+            entity1.ListOfSubEntities.Add(sub14);
+            entity1.ListOfSubEntities.Add(sub15);
+            entity1.ListOfSubEntities.Add(sub16);
+            entity1.ListOfSubEntities.Add(sub17);
+            entity1.ListOfSubEntities.Add(sub18);
+            entity1.ListOfSubEntities.Add(sub19);
+            entity1.ListOfSubEntities.Add(sub20);
+            entity1.ListOfSubEntities.Add(sub21);
+            entity1.ListOfSubEntities.Add(sub22);
+            entity1.ListOfSubEntities.Add(sub23);
+            entity1.ListOfSubEntities.Add(sub24);
+            entity1.ListOfSubEntities.Add(sub25);
+            return entity1;
+        }
+
+        public static async Task AddToMongo()
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Random");
+            var collection = database.GetCollection<Entity>("Random");
+            await collection.InsertOneAsync(CreateEntity());
+        }
+
+        public static async Task GetAllFromMongo()
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Random");
+            var collection = database.GetCollection<Entity>("Random");
+            var list = await collection.Find(x => true).ToListAsync();
+
+            foreach (var item in list)
+            {
+                System.Console.WriteLine($"{item.NameOfEntity}");
+            }
+        }
+    }
+}
---
+++ Random/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Random
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            AddToMongo().GetAwaiter();
+        }
+        public static async Task AddToMongo()
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Random");
+            var collection = database.GetCollection<Entity>("Random");
+            await collection.InsertOneAsync(MongoExamples.CreateEntity());
+        }
+
+        public async Task GetAllFromMongo()
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Random");
+            var collection = database.GetCollection<Entity>("Random");
+            var list = await collection.Find(x => true).ToListAsync();
+
+            foreach (var item in list)
+            {
+                txt_Name.Text += item.;
+            }
+        }
+        private void btn_rnd_Click(object sender, RoutedEventArgs e)
+        {
+            GetAllFromMongo().GetAwaiter();
+        }
+    }
+}
