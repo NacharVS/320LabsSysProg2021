768a4c8187cc554f6ec9819c54f9dc218d066448
Даниль Фахрутдинов <danilfahrutdinov357@mail.ru>
2022-02-18 13:49:59 +0300

    Create wpf-async

---
+++ WpfAsync/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAsync
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+        private async void btn_add(object sender, RoutedEventArgs e)
+        {
+            var rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                await Task.Delay(1000);
+                Dispatcher.Invoke(() => Numbers.Items.Add(rnd.Next(10, 1000)));
+            }
+
+        }
+    }
+}
