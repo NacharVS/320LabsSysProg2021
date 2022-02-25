fcaa3ba7892320832a2b6774974e7324eb8216a3
Михаил Мясников <gutufik@mail.ru>
2022-02-25 10:49:09 +0300

    Add Edit student method

---
+++ DutyGenerator/EditPersonWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace DutyGenerator
+{
+    /// <summary>
+    /// Логика взаимодействия для EditPersonWindow.xaml
+    /// </summary>
+    public partial class EditPersonWindow : Window
+    {
+        public List<Student> Students;
+        public EditPersonWindow()
+        {
+            InitializeComponent();
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("320Group");
+            var collection = database.GetCollection<Student>("320Group");
+            Students = collection.Find(x => true).ToList();
+        }
+    }
+}
--- DutyGenerator/MainWindow.xaml.cs
+++ DutyGenerator/MainWindow.xaml.cs
@@ -24,12 +24,12 @@ namespace DutyGenerator
     public partial class MainWindow : Window
     {
         List<Student> Students { get; set; }
+        private Student student;
+
         public MainWindow()
         {
-            //MongoExamples.AddToMongo().GetAwaiter();
-            
             InitializeComponent();
-            MongoExamples.AddToMongo(new Student() { FirstName = "Михаил", LastName = "Бозин" }).GetAwaiter();
+            //MongoExamples.AddToMongo(new Student() { FirstName = "Михаил", LastName = "Бозин" }).GetAwaiter();
         }
 
         private async void Button_Click(object sender, RoutedEventArgs e)
@@ -40,12 +40,50 @@ namespace DutyGenerator
             var list = await collection.Find(x => true).ToListAsync();
             var rnd = new Random();
             var first = rnd.Next(0, 18);
-            
+
             FirstStudent.Text = $"{list[first].FirstName} {list[first].LastName}";
             list.RemoveAt(first);
 
             var second = rnd.Next(0, 17);
             SecondStudent.Text = $"{list[second].FirstName} {list[second].LastName}";
+        }
+
+        private async void btnFillList_Click(object sender, RoutedEventArgs e)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("320Group");
+            var collection = database.GetCollection<Student>("320Group");
+            var list = await collection.Find(x => true).ToListAsync();
+            var rnd = new Random();
+            var first = rnd.Next(0, 18);
+
+            lbStudents.ItemsSource = list;
+        }
+
+        private void lbStudents_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lbStudents.SelectedItem as Student != null)
+            {
+                student = lbStudents.SelectedItem as Student;
+
+                tbFirstName.Text = student.FirstName;
+                tbLastName.Text = student.LastName;
+            }    
+        }
+
+        private async void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            var std = new MongoClient("mongodb://localhost");
+            var database = std.GetDatabase("320Group");
+            var collection = database.GetCollection<Student>("320Group");
+
+            var filter = Builders<Student>.Filter.Eq("_id", student._id);
+            var updateName = Builders<Student>.Update.Set(x => x.FirstName, tbFirstName.Text);
+            var updateSurname = Builders<Student>.Update.Set(x => x.LastName, tbLastName.Text);
+            collection.UpdateOne(filter, updateSurname);
+            collection.UpdateOne(filter, updateName);
+            lbStudents.ItemsSource = await collection.Find(x => true).ToListAsync();
+            MessageBox.Show("Студент переименоват");
         }
     }
 
@@ -56,18 +94,9 @@ namespace DutyGenerator
         public ObjectId _id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-    }
-
-    class MongoExamples
-    {
-
-        public static async Task AddToMongo(Student student)
+        public override string ToString()
         {
-            var client = new MongoClient("mongodb://localhost");
-            var database = client.GetDatabase("320Group");
-            var collection = database.GetCollection<Student>("320Group");
-            //await collection.InsertOneAsync(student);
-            await collection.ReplaceOneAsync(new BsonDocument("FirstName", "Михаил"), student);
+            return $"{FirstName} {LastName}";
         }
     }
 }
