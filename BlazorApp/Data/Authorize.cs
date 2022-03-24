894db7311eca31495ba6d4f29860d541ab714bc8
JustMe17Artem <sematkinartem6@gmail.com>
2022-03-24 10:21:52 +0300

    blazor

---
+++ BlazorApp/Data/Authorize.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components.Web;
+
+
+namespace WebApp.Data
+{
+    public class Authorize : ModalTemplate
+    {
+        protected override RenderFragment Header()
+        {
+            return (builder) =>
+            {
+                builder.AddContent(1, "Авторизация");
+            };
+        }
+
+        protected override RenderFragment Body()
+        {
+            return (builder) =>
+            {
+
+                builder.AddContent(1, "Login");
+                builder.OpenElement(2, "div");
+                builder.OpenElement(3, "input");
+                builder.CloseElement();
+                builder.CloseElement();
+                builder.AddContent(4, "Password");
+                builder.OpenElement(5, "div");
+                builder.OpenElement(6, "input");
+                builder.CloseElement();
+                builder.CloseElement();
+
+
+            };
+        }
+
+        protected override RenderFragment Footer()
+        {
+            int seq = 0;
+            return (builder) =>
+            {
+                builder.OpenElement(++seq, "button");
+                builder.AddAttribute(++seq, "type", "button");
+                builder.AddAttribute(++seq, "class", "btn btn-success");
+                builder.AddAttribute(++seq, "data-dismiss", "modal");
+                builder.AddAttribute(++seq, "onclick", EventCallback.Factory.Create<MouseEventArgs>(this, Close));
+                builder.AddContent(++seq, "Автортизоваться");
+                builder.CloseElement();
+            };
+        }
+    }
+}
