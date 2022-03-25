14a406c1b54702cea135dd9bacfdcf671c901904
1318PK <ainur.hak2004@gmail.com>
2022-03-25 14:01:12 +0300

    Authorization and regestration (blazor)

---
+++ AuthorizReg/Data/Register.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace AuorizRegister.Data
+{
+    public class Register : Authoriz
+    {
+        protected override RenderFragment Header()
+        {
+            return (builder) =>
+            {
+                builder.AddContent(1, "Авторизация");
+            };
+        }
+        protected override RenderFragment Body()
+        {
+            int seq = 0;
+            return (builder) =>
+            {
+                builder.OpenElement(++seq, "div");
+                builder.AddAttribute(++seq, "style", "display: flex; justify-content: space-between; margin-bottom: 10px");
+                builder.AddContent(++seq, "E-mail:");
+                builder.OpenElement(++seq, "input");
+                builder.AddAttribute(++seq, "style", "margin-right: 30px");
+                builder.CloseElement();
+                builder.CloseElement();
+
+                builder.OpenElement(++seq, "div");
+                builder.AddAttribute(++seq, "style", "display: flex; justify-content: space-between; margin-bottom: 10px");
+                builder.AddContent(++seq, "Login:");
+                builder.OpenElement(++seq, "input");
+                builder.AddAttribute(++seq, "style", "margin-right: 30px");
+                builder.CloseElement();
+                builder.CloseElement();
+
+                builder.OpenElement(++seq, "div");
+                builder.AddAttribute(++seq, "style", "display: flex; justify-content: space-between; margin-bottom: 10px");
+                builder.AddContent(++seq, "Password:");
+                builder.OpenElement(++seq, "input");
+                builder.AddAttribute(++seq, "style", "margin-right: 30px");
+                builder.CloseElement();
+                builder.CloseElement();
+
+                builder.OpenElement(++seq, "div");
+                builder.AddAttribute(++seq, "style", "display: flex; justify-content: space-between; margin-bottom: 10px");
+                builder.AddContent(++seq, "Repeat password:");
+                builder.OpenElement(++seq, "input");
+                builder.AddAttribute(++seq, "style", "margin-right: 30px");
+                builder.CloseElement();
+                builder.CloseElement();
+            };
+        }
+    }
+}
