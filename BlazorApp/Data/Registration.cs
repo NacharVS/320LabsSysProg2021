49a20a6c3b67ebc2d4e01ffd8dbcd2cbe013f632
Danis Mavlin <mavlindanis@gmail.com>
2022-03-25 16:02:50 +0300

    blazor

---
+++ BlazorApp/Data/Registration.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace BlazorApp.Data
+{
+    public class Registration : Authorization
+    {
+        protected override RenderFragment Header()
+        {
+            return (builder) =>
+            {
+                builder.AddContent(1, "Регистрация");
+            };
+        }
+
+        protected override RenderFragment Body()
+        {
+            return (builder) =>
+            {
+                builder.AddContent(1, "Логин ");
+                builder.OpenElement(2, "div");
+                builder.OpenElement(3, "input");
+                builder.CloseElement();
+                builder.CloseElement();
+                builder.AddContent(4, "Пароль");
+                builder.OpenElement(5, "div");
+                builder.OpenElement(6, "input");
+                builder.CloseElement();
+                builder.CloseElement();
+                builder.AddContent(7, " Подтвердите пароль");
+                builder.OpenElement(8, "div");
+                builder.OpenElement(9, "input");
+                builder.CloseElement();
+                builder.CloseElement();
+                builder.AddContent(10, "Телефон");
+                builder.OpenElement(11, "div");
+                builder.OpenElement(12, "input");
+                builder.CloseElement();
+                builder.CloseElement();
+
+            };
+        }
+
+        protected override RenderFragment Footer()
+        {
+            int seq = 0;
+            return (builder) =>
+            {
+                builder.OpenElement(++seq, "button");
+                builder.AddAttribute(++seq, "type", "button");
+                builder.AddAttribute(++seq, "class", "btn btn-success");
+                builder.AddAttribute(++seq, "data-dismiss", "modal");
+                builder.AddAttribute(++seq, "onclick", EventCallback.Factory.Create<MouseEventArgs>(this, Close));
+                builder.AddContent(++seq, "Ok");
+                builder.CloseElement();
+            };
+        }
+    }
+}
---
+++ BlazorApp/Data/User.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace BlazorApp.Data
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Role { get; set; }
+
+        public static List<User> GetUserList()
+        {
+            List<User> lis = new List<User>()
+            {
+                 new User { FirstName = "Frank", LastName = "Murphy", Email = "frank.murphy@test.com", Role = "User" },
+                 new User { FirstName = "Vic", LastName = "Reynolds", Email = "vic.reynolds@test.com", Role = "Admin"},
+                 new User { FirstName = "Gina", LastName = "Jabowski", Email = "gina.jabowski@test.com", Role = "Admin"},
+                 new User { FirstName = "Jessi", LastName = "Glaser", Email = "jessi.glaser@test.com", Role = "User"},
+                 new User { FirstName = "Jay", LastName = "Bilzerian", Email = "jay.bilzerian@test.com", Role = "User"},
+            };
+
+            return lis;
+        }
+    }
+}
