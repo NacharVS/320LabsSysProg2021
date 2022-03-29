34576d66a48c986d9d21f24dcf74794ed8ca156d
Михаил Мясников <gutufik@mail.ru>
2022-03-29 14:27:55 +0300

    Added random cars

--- BlazorTestt/Data/CarSerice.cs
+++ BlazorTestt/Data/CarSerice.cs
@@ -9,19 +9,21 @@ namespace BlazorTestt.Data
     {
         private static readonly string[] Names = new[]
         {
-            "Golden Eagle", "Stels ATV 300B", "Regulmoto sport 003 PR", "Regulmoto sport 003 PR", "Гриззли",
-            "Avantis Hunter LUX New", "Honda PCX125", "Vento retro 49cc", "Honda CMX1100A", "Pulsar NS 125"
+            "Kia", "BMW", "Citroen", "Skoda", "Audi"
+        };
+        private static readonly string[] Models = new[]
+        {
+            "Rio","5", "Jumper", "Octavia", "A6"
         };
 
-        public Task<Motorcycle[]> GetMotorcycleAsync()
+        public Task<Car[]> GetCarsAsync()
         {
             var rnd = new Random();
-            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new Motorcycle
+            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new Car
             {
                 Name = Names[rnd.Next(Names.Length)],
-                Mileage = rnd.Next(0, 100000),
-                EngineCapacity = rnd.Next(10, 3000),
-                Price = rnd.Next(10, 10000000)
+                Model = Models[rnd.Next(Models.Length)],
+                Price = rnd.Next(10, 10000000),
             }).ToArray());
         }
     }
