3f70034be846eada1c301c634aec2c74dd0ca0c4
JustMe17Artem <sematkinartem6@gmail.com>
2022-03-29 12:26:27 +0300

    tables edited

--- BlazorApp/Data/Student.cs
+++ BlazorApp/Data/Student.cs
@@ -14,15 +14,29 @@ namespace WebApp.Data
         public string Group { get; set; }
 
 
-        public static List<Student> GetStudents()
+        public static List<Student> GetStudentsGroup1()
         {
             List<Student> students = new List<Student>()
             {
-                 new Student { FirstName = "Kenneth ", LastName = "Hopkins", Group = "11232"  },
-                 new Student { FirstName = "Stephen ", LastName = "Thompson", Group = "2313123" },
-                 new Student { FirstName = "Dwayne ", LastName = "Jackson", Group = "123132" },
-                 new Student { FirstName = "JesGlen ", LastName = "Mooresi", Group = "3454535" },
-                 new Student { FirstName = "Timothy ", LastName = "Murphy", Group = "1312313"},
+                 new Student { FirstName = "Kenneth ", LastName = "Hopkins", Group = "111"  },
+                 new Student { FirstName = "Stephen ", LastName = "Thompson", Group = "111" },
+                 new Student { FirstName = "Dwayne ", LastName = "Jackson", Group = "111" },
+                 new Student { FirstName = "Jes", LastName = "Mooresi", Group = "111" },
+                 new Student { FirstName = "Timothy ", LastName = "Murphy", Group = "111"},
+            };
+
+            return students;
+        }
+
+        public static List<Student> GetStudentsGroup2()
+        {
+            List<Student> students = new List<Student>()
+            {
+                 new Student { FirstName = "Jeffrey", LastName = "Reyes", Group = "222"  },
+                 new Student { FirstName = "Lawrence", LastName = "Johnson", Group = "222" },
+                 new Student { FirstName = "Jacob", LastName = "Simpson", Group = "222" },
+                 new Student { FirstName = "James", LastName = "McCoy", Group = "222" },
+                 new Student { FirstName = "Anthony", LastName = "Osborne", Group = "222"},
             };
 
             return students;
